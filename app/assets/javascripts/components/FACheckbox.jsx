252e2958b42bb8780155810f68c6435762446bea
Sergey Hanchar <hanchar.sergey@gmail.com>
2016-05-14 23:07:43 +0300

    try react

--- app/assets/javascripts/components/FACheckbox.jsx
+++ app/assets/javascripts/components/FACheckbox.jsx
@@ -3,46 +3,42 @@ class FACheckbox extends React.Component {
   constructor(props) {
     super(props)
 
+    // Define state variables
+    //-------------------------------------------------------------------------
     this.state = {
       isChecked: false
     }
 
+    // Bind methods
+    //-------------------------------------------------------------------------
     Array.from([
-      "_check",
-      "_uncheck",
-      "_classNameForChecked",
-      "_classNameForUnchecked"
+      "_click",
+      "_className"
     ]).forEach( (method) =>
       this[method] = this[method].bind(this)
     )
   }
 
-  _check() {
-    this.setState({ isChecked: true })
+  // Toggle checkbox
+  //---------------------------------------------------------------------------
+  _click() {
+    this.setState({ isChecked: !this.state.isChecked })
   }
 
-  _uncheck() {
-    this.setState({ isChecked: false })
+  // Return proper checkbox icon
+  //---------------------------------------------------------------------------
+  _className() {
+    className = ["fa"]
+
+    className.push(
+      this.state.isChecked ? "fa-check-square-o" : "fa-square-o"
+    )
+
+    return className.join(' ')
   }
 
-  _classNameForChecked() {
-    className = "fa fa-check-square-o"
-
-    if (!this.state.isChecked)
-      className += " hidden"
-
-    return className
-  }
-
-  _classNameForUnchecked() {
-    className = "fa fa-square-o"
-
-    if (this.state.isChecked)
-      className += " hidden"
-
-    return className
-  }
-
+  // Render component
+  //---------------------------------------------------------------------------
   render() {
     return (
       <div>
@@ -54,17 +50,11 @@ class FACheckbox extends React.Component {
 
         <i
           style={{ cursor: "pointer" }}
-          className={this._classNameForUnchecked()}
-          onClick={this._check}>
-        </i>
-
-        <i
-          style={{ cursor: "pointer" }}
-          className={this._classNameForChecked()}
-          onClick={this._uncheck}>
+          className={this._className()}
+          onClick={this._click}>
         </i>
       </div>
-    );
+    )
   }
 }
 
